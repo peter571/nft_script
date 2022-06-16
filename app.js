cf8b5a6d2b8850b338a69539052ee9eeca0e0f78
Koecha <koecha@localhost.localdomain>
2022-06-16 15:34:50 +0300

    cors update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config'
 import path from 'path';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 import getHolders from './npxs.js';
 
@@ -8,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors({ origin: '*' }));
 
 const intervalFunc = setInterval(() => {
   getHolders();
